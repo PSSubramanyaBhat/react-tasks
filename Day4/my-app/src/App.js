84c33a054c443cac34961fbaa1f48bf55932c573
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-10-09 02:36:26 +0530

    Added a new react app via npm commands

---
+++ Day4/my-app/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import logo from './logo.svg';
+import logo from './sasuke.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="circular-image-avatar" alt="logo" width="200" height="250"/>
+        <p>
+          My name is Sasuke Uchiha
+        </p>
+        <a
+          className="App-link"
+          // href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=_ohUaEmkngs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Have a glimpse of me...
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
